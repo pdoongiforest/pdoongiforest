f847e92db3bf4a9afb6f2f523b6cfa676d539692
EunbinJung <ainatube39@gmail.com>
2025-08-13 21:37:30 +0900

    chore/프리티어 개행

--- src/pages/Mypage/components/EditPencil.tsx
+++ src/pages/Mypage/components/EditPencil.tsx
@@ -3,26 +3,21 @@ import S from './editPencil.module.css';
 import { useCallback } from 'react';
 
 interface Props {
-    onClick: () => void;
+  onClick: () => void;
 }
 
-function EditPencil({onClick}: Props) {
-
-    const handleFunction = useCallback(() => {
-        onClick();
-    }, [onClick])
+function EditPencil({ onClick }: Props) {
+  const handleFunction = useCallback(() => {
+    onClick();
+  }, [onClick]);
 
   return (
-    <>              
-        <button
-            type="button"
-            className={S.button}
-            onClick={ handleFunction }
-        >
-            <img src={Pencil} /> 
-        </button>
+    <>
+      <button type="button" className={S.button} onClick={handleFunction}>
+        <img src={Pencil} />
+      </button>
     </>
-  )
+  );
 }
 
-export default EditPencil
+export default EditPencil;
