51a35d49252dbb5c057a5b383d2b55f336c802fc
YooJeong01 <ujjh77@naver.com>
2025-08-16 19:51:27 +0900

    refactor. 마이페이지 스크랩, 포스트 공통 영역 커스텀 훅, 컴포넌트 분리
    - 테이블에서 1차로 데이터 패치하는 부분 공통되어 useFetchUserData로 커스텀 훅을 만들어 분리했습니다
    - 렌더링하는 부분의 구조가 동일하여 RenderBoardList로 컴포넌트를 만들어 분리했습니다
    - 두 개로 나뉘었던 css를 하나로 합쳤습니다

---
+++ src/pages/Mypage/components/RenderBoardList.tsx
@@ -0,0 +1,55 @@
+import type { Database } from '@/supabase/database.types';
+import S from './MypageBoardList.module.css';
+import { Link } from 'react-router-dom';
+
+type Board = Pick<Database['public']['Tables']['board']['Row'], 'board_id'|'title'|'contents'>;
+
+interface Props {
+  category : string,
+  data : Board[] | null
+}
+
+function RenderBoardList({category, data}:Props) {
+  const translate = category === 'post' ? '포스트' : '스크랩'
+  return (
+    <>
+    <h2 className={S.sectionName}>{ translate }</h2>
+    { data && data.length>0 ? (
+      <section className={S.container}>
+          <ul className={S.list}>
+            {
+              data.map(({ board_id, title, contents }) => (
+                <li key={board_id} className={S.linkList}>
+                  <Link to={`/channel/${board_id}`}>
+                    <div className={S.title}>{title}</div>
+                    <div className={S.content}>{contents}</div>
+                  </Link>
+                </li>
+              ))}
+          </ul>
+        </section>
+
+    ) : (
+        <div className={S.nothing}>
+          <img src="/images/emptyContents.png" alt={`${translate} 없음`} />
+          {
+            category === 'post' ? (
+              <p>
+                아직 작성한 포스트가 없습니다 <br />
+                스터디에서 모집 글을 작성해보세요!
+              </p>
+            ) : (
+              <p>
+                아직 스크랩된 글이 없습니다 <br />
+                스터디에서 글을 스크랩해서 한 눈에 확인해보세요!
+              </p>
+            )
+          }
+        </div>
+    )
+      
+    }
+  </>
+  )
+}
+export default RenderBoardList
