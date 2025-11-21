80568e0c46d73a2209a8fbe31a3f12ad4fd1675c
mtm1018 <mtm1018@naver.com>
2025-11-21 12:11:36 +0900

    refactor/피어리뷰 기능구현 밸리데이션

--- src/pages/team/peerReview/PeerReview.tsx
+++ src/pages/team/peerReview/PeerReview.tsx
@@ -2,14 +2,32 @@ import { useAuth } from '@/features/auth/AuthProvider';
 import PeerReviewTemplate from '@/features/team/components/peerReview/PeerReviewTemplate';
 import { PeerReviewProvider } from '@/features/team/context/peerReviewContext';
 import type { TeamOutlet } from '@/features/team/types/types';
-import { useState } from 'react';
-import { useOutletContext } from 'react-router-dom';
+import supabase from '@/supabase/supabase';
+import { useEffect, useState } from 'react';
+import { useNavigate, useOutletContext, useParams } from 'react-router-dom';
 
 function PeerReview() {
   const { members } = useOutletContext<TeamOutlet>();
   const { profileId } = useAuth();
   const [currentIndex, setCurrentIndex] = useState(0);
   const member = members.filter((member) => member.profile_id !== profileId);
+  const { id } = useParams();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    const fetch = async () => {
+      const { data } = await supabase.from('peer_review').select('writer_id').eq('study_id', id);
+      if (data) {
+        const hasWritten = data.some((review) => review.writer_id == profileId);
+
+        if (hasWritten) {
+          alert('이미 피어리뷰를 작성하셨습니다.');
+          navigate(`/team/${id}`);
+        }
+      }
+    };
+    fetch();
+  }, [id, profileId]);
 
   return (
     <PeerReviewProvider memberId={members[currentIndex].profile_id}>
